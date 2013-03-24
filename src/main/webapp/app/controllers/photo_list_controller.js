00ba12679ac122b98c7b35c32fe17cda6ff3d2dc
Joachim Haagen Skeie <joachim@haagen.name>
2013-03-24 17:16:17 +0100

    Upgrading fully to Ember Master build

--- src/main/webapp/app/controllers/photo_list_controller.js
+++ src/main/webapp/app/controllers/photo_list_controller.js
@@ -8,12 +8,8 @@ EME.PhotosController = Ember.ArrayController.extend({
 
     nextPhoto: function() {
         var selectedPhoto = null;
-        console.log(this.get('controllers.photosSelectedPhoto'));
-        console.log(this.get('controllers.photosSelectedPhoto.content'));
         if (!this.get('controllers.photosSelectedPhoto.content')) {
-            console.log('controllers.photosSelectedPhoto.content is null');
-            selectedPhoto = this.get('content.firstObject');
-            this.transitionToRoute("photos.selectedPhoto", selectedPhoto);
+            this.transitionToRoute("photos.selectedPhoto", this.get('content.firstObject'));
         } else {
             console.log('finding next photo');
             var selectedIndex = this.findSelectedItemIndex();
@@ -30,27 +26,27 @@ EME.PhotosController = Ember.ArrayController.extend({
 
     prevPhoto: function() {
         console.log('PhotoListController prevPhoto');
-        if (!this.get('selectedPhoto')) {
-            this.set('selectedPhoto', this.get('controllers.photos.content').get('lastObject'));
+        if (!this.get('controllers.photosSelectedPhoto.content')) {
+            this.transitionToRoute("photos.selectedPhoto", this.get('content.lastObject'));
         } else {
             var selectedIndex = this.findSelectedItemIndex();
 
             if (selectedIndex <= 0) {
-                selectedIndex = this.get('controllers.photos.content').get('length') - 1;
+                selectedIndex = this.get('content.length') - 1;
             } else {
                 selectedIndex--;
             }
 
-            this.set('selectedPhoto', this.get('controllers.photos.content').objectAt(selectedIndex));
+            this.transitionToRoute("photos.selectedPhoto", this.get('content').objectAt(selectedIndex))
         }
     },
 
     findSelectedItemIndex: function() {
         var content = this.get('content');
-        var selectedPhoto = this.get('controllers.selectedPhoto.content');
+        var selectedPhoto = this.get('controllers.photosSelectedPhoto.content');
 
         for (index = 0; index < content.get('length'); index++) {
-            if (this.get('selectedPhoto') === content.objectAt(index)) {
+            if (this.get('controllers.photosSelectedPhoto.content') === content.objectAt(index)) {
                 return index;
             }
         }
